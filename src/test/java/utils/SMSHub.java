cca0f95a2e6b0f5f601471bad3101de3ec01cdff
shingelevich <63087390+poploveok@users.noreply.github.com>
2023-01-04 18:25:33 +0300

    added build.gradle rest assured5.3.0

---
+++ src/test/java/utils/SMSHub.java
@@ -0,0 +1,15 @@
+package utils;
+
+public class SMSHub {
+
+    private final String baseUrl = "https://smshub.org/stubs/handler_api.php";
+    private final String keyAPI = "164971U58d61f2ca76e311a68e606f1d6413361";
+
+    /*public String getAccountBalance(){
+        String body
+
+
+    }*/
+
+
+}
--- src/test/java/utils/SMSUtils.java
+++ src/test/java/utils/SMSUtils.java
@@ -1,4 +1,54 @@
 package utils;
 
+import com.google.gson.reflect.TypeToken;
+import org.jetbrains.annotations.NotNull;
+//import ru.sms_activate.SMSActivateAction;
+import ru.sms_activate.*;
+import ru.sms_activate.error.SMSActivateUnknownException;
+import ru.sms_activate.error.base.SMSActivateBaseException;
+import ru.sms_activate.response.api_activation.SMSActivateActivation;
+import ru.sms_activate.response.api_activation.SMSActivateGetFullSmsResponse;
+import ru.sms_activate.response.api_rent.SMSActivateGetRentListResponse;
+import ru.sms_activate.response.api_rent.SMSActivateGetRentStatusResponse;
+
+import java.math.BigDecimal;
+import java.util.List;
+
 public class SMSUtils {
+
+String apiKey =  "7424Adff2b7241e6b15e1cbdfdf25773";
+  int rentID = 8148860;
+SMSActivateApi managerSMS = new SMSActivateApi(apiKey);
+
+    public static void main(String[] args) throws SMSActivateBaseException {
+        SMSUtils smsUtils = new SMSUtils();
+        System.out.println("getRentList " +smsUtils.getRentList());
+        System.out.println("getBalance " + smsUtils.getBalance());
+    }
+
+
+    public String getRentList() throws SMSActivateBaseException {
+        SMSActivateGetRentListResponse rentListResponse = managerSMS.getRentList();
+        return rentListResponse.toString();
+    }
+
+    public BigDecimal getBalance() throws SMSActivateBaseException {
+        return managerSMS.getBalance();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+/*TODO
+*  Returns the list sms.
+* */
