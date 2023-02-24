eef8bd1a0eac4bfebafe35b1d68eb1a597160279
alx.krw <sashkir7@mail.ru>
2023-02-24 15:34:18 +0600

    Work with pdf

---
+++ src/test/java/ru/gasworkers/dev/pdf/AgreementPdfTest.java
@@ -0,0 +1,17 @@
+package ru.gasworkers.dev.pdf;
+
+import org.junit.jupiter.api.Test;
+
+public class AgreementPdfTest {
+
+    private final AgreementPdfChecker pdfChecker = AgreementPdfChecker.newInstance("src/test/resources/pdf.pdf");
+
+    @Test
+    void checkAgreementPdf() {
+//        pdfChecker.checkSection(AgreementPdfChecker.Section.AGREEMENT_SUBJECT);
+//        pdfChecker.checkSection(AgreementPdfChecker.Section.OBLIGATIONS);
+
+        pdfChecker.checkAllSections();
+    }
+
+}
