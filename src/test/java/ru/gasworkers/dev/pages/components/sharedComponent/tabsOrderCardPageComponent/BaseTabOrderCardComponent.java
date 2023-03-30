edbaf187b37f412c92a24bc157446cc7eeea945a
shingelevich <63087390+poploveok@users.noreply.github.com>
2023-03-31 00:27:18 +0300

    roleSE profile validation expanded fix

--- src/test/java/ru/gasworkers/dev/pages/components/sharedComponent/tabsOrderCardPageComponent/BaseTabOrderCardComponent.java
+++ src/test/java/ru/gasworkers/dev/pages/components/sharedComponent/tabsOrderCardPageComponent/BaseTabOrderCardComponent.java
@@ -37,39 +37,6 @@ public abstract class BaseTabOrderCardComponent extends BaseComponent {
         });
     }
 
-
-
-    public void checkErrorMsgInBox3(SelenideElement element, String errorMsg) {
-        stepWithRole("Убедиться, что присутствует сообщение об ошибке у поля: " + element.getAlias(), () -> {
-            SelenideElement sibling = element.sibling(0);
-            boolean errorMsgFound = ( element.shouldHave(text(errorMsg)).isDisplayed() ||
-                     sibling.exists() && sibling.shouldHave(text(errorMsg)).isDisplayed());
-            if (!errorMsgFound) {
-                throw new AssertionError("Ошибка! Сообщение об ошибке '" + errorMsg + "' не найдено " +
-                        "у поля " + element.getAlias() + " или его соседа.");
-            }
-        });
-    }
-
-
-
-    public void checkErrorMsgInBox2(SelenideElement element, String errorMsg) {
-        // todo list of text in the placeholder - to specify the error message
-        stepWithRole("Убедиться, что присутствует сообщение об ошибке у поля: " + element.getAlias(), () -> {
-            //check that the error message is displayed or inside the element or in the .sibling(0)
-            for (int i = 0; i < 2; i++) {
-                if (Objects.equals(element.parent().sibling(0).text(), errorMsg)) {
-                    break;
-                }
-                if (Objects.equals(element.parent().text(), errorMsg)) {
-                    break;
-                }
-                element.parent().sibling(0).shouldHave(text(errorMsg));
-            }
-
-        });
-    }
-
     public void checkNoErrorMsgInBox(SelenideElement element) {
         stepWithRole("Убедиться, что отсутствует сообщение об ошибке поле не заполнено у поля: " + element.getAlias(), () -> {
             element.parent().sibling(0).shouldNot(exist);
