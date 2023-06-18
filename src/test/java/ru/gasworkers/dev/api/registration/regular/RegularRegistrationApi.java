658c4eaaa7a7013642d5ba3b621e232df60310f3
shingelevich <63087390+poploveok@users.noreply.github.com>
2023-06-18 15:03:53 +0300

    upd LoginApiTest and it components

--- src/test/java/ru/gasworkers/dev/api/registration/regular/RegularRegistrationApi.java
+++ src/test/java/ru/gasworkers/dev/api/registration/regular/RegularRegistrationApi.java
@@ -1,8 +1,12 @@
 package ru.gasworkers.dev.api.registration.regular;
 
 import io.qameta.allure.Step;
+import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
+import lombok.Getter;
+import lombok.Setter;
 import ru.gasworkers.dev.api.BaseApi;
+import ru.gasworkers.dev.api.authorisation.LoginRequestDTO;
 import ru.gasworkers.dev.api.registration.regular.dto.ComplexRegistrationRequestDto;
 import ru.gasworkers.dev.api.registration.regular.dto.check.CheckRegistrationRequestDto;
 import ru.gasworkers.dev.api.registration.regular.dto.finish.FinishRegistrationRequestDto;
@@ -10,27 +14,29 @@ import ru.gasworkers.dev.api.registration.regular.dto.start.StartRegistrationReq
 
 import static io.restassured.RestAssured.given;
 
+
 public class RegularRegistrationApi extends BaseApi {
+    @Getter
+    @Setter
+    private static String loginToken;
+
     //-------------------------------------COMPLEX REGISTRATION---------------------------------------------------------
     @Step("API: Комплексная регулярная регистрация")
     public void complexRegistration(ComplexRegistrationRequestDto complexDto) {
         startRegistration(complexDto.toStartRegistration()).statusCode(200);
         checkRegistration(complexDto.toCheckRegistration()).statusCode(200);
         finishRegistration(complexDto.toFinishRegistration()).statusCode(200);
+        login(complexDto.toLogin()).statusCode(200);
     }
-
-
 
     //--------------------------------------------START-----------------------------------------------------------------
     @Step("API: Регулярная регистрация [Начало]")
     public ValidatableResponse startRegistration(StartRegistrationRequestDto inputDto) {
         return given().spec(baseRequestSpec)
-                // do not understand how object is converted to json
                 .body(inputDto)
                 .when()
                 .post("/auth/register")
                 .then().spec(baseResponseSpec);
-
     }
 
     public ValidatableResponse startRegistration(String type, String email, String phone, Boolean isPhoneSend) {
@@ -64,4 +70,22 @@ public class RegularRegistrationApi extends BaseApi {
                 .then().spec(baseResponseSpec);
     }
 
+    //----------------------------------------------LOGIN----------------------------------------------------------------
+    @Step("API: Регулярная регистрация [Логин]")
+    public ValidatableResponse login(LoginRequestDTO inputDto) {
+        Response response = given().spec(baseRequestSpec)
+                .body(inputDto)
+                .when()
+                .post("/auth/login")
+                .then().spec(baseResponseSpec)
+                .extract().response();
+
+        // Extract the token from the response body
+        String token = response.jsonPath().getString("data.token");
+
+        // Store the token for later use
+        setLoginToken(token);
+
+        return response.then().spec(baseResponseSpec);
+    }
 }
--- src/test/java/ru/gasworkers/dev/tests/api/authorization/LoginApiTest.java
+++ src/test/java/ru/gasworkers/dev/tests/api/authorization/LoginApiTest.java
@@ -11,22 +11,25 @@ import ru.gasworkers.dev.allure.AllureEpic;
 import ru.gasworkers.dev.allure.AllureTag;
 import ru.gasworkers.dev.api.authorisation.LoginApi;
 import ru.gasworkers.dev.api.registration.regular.RegularRegistrationApi;
+import ru.gasworkers.dev.extension.user.User;
+import ru.gasworkers.dev.extension.user.WithUser;
 import ru.gasworkers.dev.tests.api.BaseApiTest;
 
 import static io.qameta.allure.Allure.step;
 
+@Owner("Igor Shingelevich")
+@Epic(AllureEpic.AUTHORIZATION)
+@Tags({@Tag(AllureTag.REGRESSION), @Tag(AllureTag.CLIENT), @Tag(AllureTag.POSITIVE), @Tag(AllureTag.API)})
+@DisplayName("Login API Test")
 public class LoginApiTest extends BaseApiTest {
     private final RegularRegistrationApi registrationApi = new RegularRegistrationApi();
     private final LoginApi loginApi = new LoginApi();
 
     @ParameterizedTest(name = "{0}")
     @EnumSource(LoginPositiveCase.class)
-    @Owner("Igor Shingelevich")
-    @Epic(AllureEpic.AUTHORIZATION)
-    @Tags({@Tag(AllureTag.REGRESSION), @Tag(AllureTag.CLIENT), @Tag(AllureTag.AUTHORISATION), @Tag(AllureTag.POSITIVE), @Tag(AllureTag.API)})
-    @DisplayName("Success case:")
-    void authorization(LoginPositiveCase testCase) {
-        step("Start registration", () ->
+    @DisplayName("Success case")
+    void authorization(LoginPositiveCase testCase, @WithUser User client) {
+       /* step("Start registration", () ->
                 registrationApi.startRegistration(testCase.getStartDto())
                         .statusCode(200));
 
@@ -35,11 +38,14 @@ public class LoginApiTest extends BaseApiTest {
                         .statusCode(200));
 
         step("Finish registration", () ->
-            registrationApi.finishRegistration(testCase.getFinishDto())
-                    .statusCode(200));
+                registrationApi.finishRegistration(testCase.getFinishDto())
+                        .statusCode(200));
 
         step("Login", () ->
-            loginApi.login(testCase.getLoginDto())
-                    .statusCode(200));
+                loginApi.login(testCase.getLoginDto())
+                        .statusCode(200));
+*/
+        String token = RegularRegistrationApi.getLoginToken();
+        System.out.println("token = " + token);
     }
 }
