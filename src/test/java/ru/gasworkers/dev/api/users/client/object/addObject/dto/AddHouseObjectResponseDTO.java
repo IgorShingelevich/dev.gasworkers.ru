d194644d408523a5ecb1dbf1189dbc0b1ea79f2f
shingelevich <63087390+poploveok@users.noreply.github.com>
2023-06-23 02:10:01 +0300

    expanded AddHouseObjectNegativeCase with corresponded methods and .json

--- src/test/java/ru/gasworkers/dev/api/users/client/object/addObject/dto/AddHouseObjectResponseDTO.java
+++ src/test/java/ru/gasworkers/dev/api/users/client/object/addObject/dto/AddHouseObjectResponseDTO.java
@@ -1,6 +1,8 @@
 package ru.gasworkers.dev.api.users.client.object.addObject.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,26 +21,23 @@ public class AddHouseObjectResponseDTO {
     @NoArgsConstructor
     @AllArgsConstructor
     public static class DataDto {
-
         private Integer id;
-
         private String title;
-
-        private Integer active_offers_count;
-
+        @JsonProperty("active_offers_count")
+        private Integer activeOffersCount;
         private Object[] photos;
-
         private AddressDto address;
-
         private Object[] equipments;
-
         private Object branch;
-
         private CompanyDto company;
-        private Object account_number;
-        private Boolean video_exists;
-        private Integer created_at;
+        @JsonProperty("account_number")
+        private Object accountNumber;
+        @JsonProperty("video_exists")
+        private Boolean videoExists;
+        @JsonProperty("created_at")
+        private Integer createdAt;
     }
+
     @Data
     @NoArgsConstructor
     @AllArgsConstructor
@@ -46,35 +45,50 @@ public class AddHouseObjectResponseDTO {
         private Integer id;
         private String full;
         private String type;
-        private String full_with_zip;
+        @JsonProperty("full_with_zip")
+        private String fullWithZip;
         private Object zip;
         private String country;
-        private String country_code;
-
+        @JsonProperty("country_code")
+        private String countryCode;
         private String region;
-        private String region_type;
-        private Object region_with_type;
+        @JsonProperty("region_type")
+        private String regionType;
+        @JsonProperty("region_with_type")
+        private Object regionWithType;
         private Object area;
-        private Object area_type;
-        private Object area_with_type;
+        @JsonProperty("area_type")
+        private Object areaType;
+        @JsonProperty("area_with_type")
+        private Object areaWithType;
         private Object city;
-        private Object city_type;
-        private Object city_with_type;
+        @JsonProperty("city_type")
+        private Object cityType;
+        @JsonProperty("city_with_type")
+        private Object cityWithType;
         private Object settlement;
-        private Object settlement_type;
-        private Object settlement_with_type;
+        @JsonProperty("settlement_type")
+        private Object settlementType;
+        @JsonProperty("settlement_with_type")
+        private Object settlementWithType;
         private Object street;
-        private Object street_type;
-        private Object street_with_type;
+        @JsonProperty("street_type")
+        private Object streetType;
+        @JsonProperty("street_with_type")
+        private Object streetWithType;
         private Object house;
-        private Object house_type;
-        private Object house_with_type;
+        @JsonProperty("house_type")
+        private Object houseType;
+        @JsonProperty("house_with_type")
+        private Object houseWithType;
         private Object block;
         private Object flat;
-        private Object capital_marker;
+        @JsonProperty("capital_marker")
+        private Object capitalMarker;
         private Double longitude;
         private Double latitude;
     }
+
     @Data
     @NoArgsConstructor
     @AllArgsConstructor
@@ -82,41 +96,157 @@ public class AddHouseObjectResponseDTO {
         private Integer id;
         private String title;
         private BranchDto[] branches;
-
     }
+
     @Data
     @NoArgsConstructor
     @AllArgsConstructor
     public static class BranchDto {
         private Integer id;
         private String title;
-
     }
+
     @Data
     @NoArgsConstructor
     @AllArgsConstructor
     public static class ErrorsDto {
-        private String[] errors;
+        @JsonProperty("address_id")
+        private String[] addressIdErrors;
+        @JsonProperty ("company_id")
+        private String[] companyIdErrors;
+        private String[] titleErrors;
+        @JsonProperty("branch_id")
+        private String[] branchIdErrors;
 
     }
-    // Success Response
+
+    @JsonIgnoreProperties(value = {"status"})
+    public static AddHouseObjectResponseDTO missingAddressId() {
+        /*{
+  "message": "Поле address id обязательно для заполнения.",
+  "errors": {
+    "address_id": [
+      "Поле address id обязательно для заполнения."
+    ]
+  }
+}*/
+        ErrorsDto errors = new ErrorsDto();
+        errors.setAddressIdErrors(new String[]{"Поле address id обязательно для заполнения."});
+
+        return new AddHouseObjectResponseDTO(1, "Поле address id обязательно для заполнения.", null, errors);
+    }
+    @JsonIgnoreProperties(value = {"status"})
+
+    public static AddHouseObjectResponseDTO missingCompanyId() {
+        /*{
+            "message": "Поле company id обязательно для заполнения.",
+            "errors": {
+                "company_id": [
+                    "Поле company id обязательно для заполнения."
+                ]
+            }
+        }*/
+        ErrorsDto errors = new ErrorsDto();
+        errors.setCompanyIdErrors(new String[]{"Поле company id обязательно для заполнения."});
+        return new AddHouseObjectResponseDTO(1, "Поле company id обязательно для заполнения.", null, errors);
+    }
+    @JsonIgnoreProperties(value = {"status"})
+    public static AddHouseObjectResponseDTO missingTitle() {
+        /*{
+            "message": "Поле Название обязательно для заполнения.",
+            "errors": {
+                "title": [
+                    "Поле Название обязательно для заполнения."
+                ]
+            }
+        }*/
+        ErrorsDto errors = new ErrorsDto();
+        errors.setTitleErrors(new String[]{"Поле Название обязательно для заполнения."});
+        return new AddHouseObjectResponseDTO(1, "Поле Название обязательно для заполнения.", null, errors);
+    }
+    public static AddHouseObjectResponseDTO missingBranchId() {
+        return new AddHouseObjectResponseDTO(1, "Не указан филиал", null, null);
+    }
+    public static AddHouseObjectResponseDTO missingAccountNumber() {
+        return new AddHouseObjectResponseDTO(1, "Не указан лицевой счет", null, null);
+    }
+    @JsonIgnoreProperties(value = {"status"})
+    public static AddHouseObjectResponseDTO invalidAddressId() {
+        /*{
+            "message": "Выбранное значение для address id некорректно.",
+            "errors": {
+                "address_id": [
+                    "Выбранное значение для address id некорректно."
+                ]
+            }
+        }*/
+        ErrorsDto errors = new ErrorsDto();
+        errors.setAddressIdErrors(new String[]{"Выбранное значение для address id некорректно."});
+        return new AddHouseObjectResponseDTO(1, "Выбранное значение для address id некорректно.", null, errors);
+    }
+    @JsonIgnoreProperties(value = {"status"})
+    public static AddHouseObjectResponseDTO invalidCompanyId() {
+        /*{
+            "message": "Выбранное значение для company id некорректно.",
+            "errors": {
+                "company_id": [
+                    "Выбранное значение для company id некорректно."
+                ]
+            }
+        }*/
+        ErrorsDto errors = new ErrorsDto();
+        errors.setCompanyIdErrors(new String[]{"Выбранное значение для company id некорректно."});
+        return new AddHouseObjectResponseDTO(1, "Выбранное значение для company id некорректно.", null, errors);
+    }
+    @JsonIgnoreProperties(value = {"status"})
+    public static AddHouseObjectResponseDTO invalidBranchId() {
+        /*{
+            "message": "Выбранное значение для branch id некорректно.",
+            "errors": {
+                "branch_id": [
+                    "Выбранное значение для branch id некорректно."
+                ]
+            }
+        }*/
+        ErrorsDto errors = new ErrorsDto();
+        errors.setBranchIdErrors(new String[]{"Выбранное значение для branch id некорректно."});
+        return new AddHouseObjectResponseDTO(1, "Выбранное значение для branch id некорректно.", null, errors);
+    }
+    public static AddHouseObjectResponseDTO invalidAccountNumber() {
+        return new AddHouseObjectResponseDTO(1, "Неверный лицевой счет", null, null);
+    }
+    @JsonIgnoreProperties(value = {"status"})
+    public static AddHouseObjectResponseDTO invalidTitle() {
+        /*{
+            "message": "Поле Название обязательно для заполнения.",
+            "errors": {
+                "title": [
+                    "Поле Название обязательно для заполнения."
+                ]
+            }
+        }*/
+        ErrorsDto errors = new ErrorsDto();
+        errors.setTitleErrors(new String[]{"Поле Название обязательно для заполнения."});
+        return new AddHouseObjectResponseDTO(1, "Поле Название обязательно для заполнения.", null, errors);
+    }
+
     public static AddHouseObjectResponseDTO successResponse() {
         DataDto data = new DataDto();
-        data.setId(null); // Set id field to null
+        data.setId(null);
         data.setTitle("my house");
-        data.setActive_offers_count(0);
+        data.setActiveOffersCount(0);
         data.setPhotos(new Object[]{});
 
         AddressDto address = new AddressDto();
         address.setId(2121);
         address.setFull("Москва, Московская улица");
         address.setType("street");
-        address.setFull_with_zip("Россия, Москва, Московская улица");
+        address.setFullWithZip("Россия, Москва, Московская улица");
         address.setZip(null);
         address.setCountry("Россия");
-        address.setCountry_code("RU");
+        address.setCountryCode("RU");
         address.setRegion("Москва");
-        address.setRegion_type("Москва");
+        address.setRegionType("Москва");
         address.setLongitude(37.416161);
         address.setLatitude(55.671229);
 
@@ -138,42 +268,10 @@ public class AddHouseObjectResponseDTO {
         company.setBranches(branches);
 
         data.setCompany(company);
-        data.setAccount_number(null);
-        data.setVideo_exists(false);
-        data.setCreated_at(null); // Set created_at field to null
+        data.setAccountNumber(null);
+        data.setVideoExists(false);
+        data.setCreatedAt(null);
 
         return new AddHouseObjectResponseDTO(0, "Объект добавлен", data, null);
-    }
-    public static AddHouseObjectResponseDTO missingAddressId() {
-        return new AddHouseObjectResponseDTO(1, "Не указан адрес", null, null);
-    }
-    public static AddHouseObjectResponseDTO missingCompanyId() {
-        return new AddHouseObjectResponseDTO(1, "Не указана компания", null, null);
-    }
-    public static AddHouseObjectResponseDTO missingTitle() {
-        return new AddHouseObjectResponseDTO(1, "Не указано название объекта", null, null);
-    }
-    public static AddHouseObjectResponseDTO missingBranchId() {
-        return new AddHouseObjectResponseDTO(1, "Не указан филиал", null, null);
-    }
-    public static AddHouseObjectResponseDTO missingAccountNumber() {
-        return new AddHouseObjectResponseDTO(1, "Не указан лицевой счет", null, null);
-    }
-    public static AddHouseObjectResponseDTO invalidAddressId() {
-        return new AddHouseObjectResponseDTO(1, "Неверный адрес", null, null);
-    }
-
-    public static AddHouseObjectResponseDTO invalidCompanyId() {
-        return new AddHouseObjectResponseDTO(1, "Неверная компания", null, null);
-    }
-    public static AddHouseObjectResponseDTO invalidBranchId() {
-        return new AddHouseObjectResponseDTO(1, "Неверный филиал", null, null);
-    }
-    public static AddHouseObjectResponseDTO invalidAccountNumber() {
-        return new AddHouseObjectResponseDTO(1, "Неверный лицевой счет", null, null);
-    }
-
-    public static AddHouseObjectResponseDTO invalidTitle() {
-        return new AddHouseObjectResponseDTO(1, "Неверное название объекта", null, null);
     }
 }
