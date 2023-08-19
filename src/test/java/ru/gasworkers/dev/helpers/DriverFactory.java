b123c1bdceaee3e60dd961e47f1489e7d3d3d3e9
shingelevich <63087390+poploveok@users.noreply.github.com>
2023-08-19 13:36:48 +0300

    added PreconditionRepair

--- src/test/java/ru/gasworkers/dev/helpers/DriverFactory.java
+++ src/test/java/ru/gasworkers/dev/helpers/DriverFactory.java
@@ -25,7 +25,7 @@ public final class DriverFactory {
         config.browserPosition(annotation.browserPosition());
         config.timeout(8000);
         config.headless(false);
-        config.holdBrowserOpen(true); // check remote
+        config.holdBrowserOpen(false); // check remote
         config.browserCapabilities(capabilities);
         ChromeOptions chromeOptions = new ChromeOptions();
         // Install and activate the extension
