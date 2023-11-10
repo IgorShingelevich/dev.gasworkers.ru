7fa78518b0d31f2bc6f72e581e73e1d63d2e7ee5
shingelevich <63087390+poploveok@users.noreply.github.com>
2023-11-10 15:32:21 +0300

    upd ActionsInvoicePaidClientRepairStateTest

--- src/test/java/ru/gasworkers/dev/api/BaseApi.java
+++ src/test/java/ru/gasworkers/dev/api/BaseApi.java
@@ -32,48 +32,4 @@ public abstract class BaseApi {
 
 }
 
- /* protected void addTestWatcher(TestScenario scenario) {
-        TestWatcher watchman = new TestWatcher() {
 
-            @Override
-            protected void failed(Throwable e, Description description) {
-                switch (scenario) {
-                    case CONSULTATION_MASTER_APPLIED:
-                        consultationCancelRequest(e, description);
-                        break;
-                    case SCENARIO_2:
-                        handleFailureScenario2(e, description);
-                        break;
-                    default:
-                        // Handle any default behavior here
-                        break;
-                }
-            }
-
-            private void consultationCancelRequest(Throwable e, Description description) { // token)
-                ConsultationCancelRequestApi consultationCancelRequestApi = new ConsultationCancelRequestApi();
-
-            }
-
-            private void handleFailureScenario2(Throwable e, Description description) {
-                // This method will be called if the test fails or throws an exception
-                // in Scenario 2
-                // Perform your desired action here
-            }
-
-        };
-
-        // Apply the rule to the current test class
-        org.junit.rules.RuleChain.outerRule(watchman);
-        // in testclass
-        *//* @Before
-    public void setup() {
-        String token = "your_token_here";
-        addTestWatcher(TestScenario.SCENARIO_1, token); // Apply Scenario 1 failure handling logic
-    }*//*
-    }*/
-
-  /* protected enum TestScenario {
-        CONSULTATION_MASTER_APPLIED,
-        SCENARIO_2
-    }*/
