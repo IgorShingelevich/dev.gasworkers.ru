eab6455c2828032e2ab7d609e0e082bae15124ce
shingelevich <63087390+poploveok@users.noreply.github.com>
2023-11-12 17:52:29 +0300

    upd BrowserCapabilities keep only chrome119

--- src/test/java/ru/gasworkers/dev/helpers/BrowserCapabilities.java
+++ src/test/java/ru/gasworkers/dev/helpers/BrowserCapabilities.java
@@ -10,11 +10,11 @@ import ru.gasworkers.dev.exception.EnumNotSupportedException;
 @Getter
 @AllArgsConstructor
 public enum BrowserCapabilities {
-    CHROME_114("chrome", "114.0"),
-    CHROME_115("chrome", "115.0"),
-    CHROME_119("chrome", "119.0"),
-    FIREFOX_114("firefox", "114.0"),
-    FIREFOX_115("firefox", "115.0");
+    //    CHROME_114("chrome", "114.0"),
+//    CHROME_115("chrome", "115.0"),
+    CHROME_119("chrome", "119.0");
+//    FIREFOX_114("firefox", "114.0"),
+//    FIREFOX_115("firefox", "115.0");
 
     private final String browserName;
     private final String browserVersion;
@@ -23,26 +23,17 @@ public enum BrowserCapabilities {
         ChromeOptions chromeOptions = new ChromeOptions();
         FirefoxOptions firefoxOptions = new FirefoxOptions();
         switch (this) {
-            case CHROME_114:
-                chromeOptions.addArguments("--use-fake-ui-for-media-stream");
-                chromeOptions.setCapability("browserVersion", browserVersion);
-                return chromeOptions;
-            case CHROME_115:
-                chromeOptions.addArguments("--use-fake-ui-for-media-stream");
-                chromeOptions.setCapability("browserVersion", browserVersion);
-                return chromeOptions;
+//            case CHROME_114:
+//            case CHROME_115:
             case CHROME_119:
                 chromeOptions.addArguments("--use-fake-ui-for-media-stream");
                 chromeOptions.setCapability("browserVersion", browserVersion);
                 return chromeOptions;
-            case FIREFOX_114:
-                firefoxOptions.addArguments("--use-fake-ui-for-media-stream");
-                firefoxOptions.setCapability("browserVersion", browserVersion);
-                return firefoxOptions;
-            case FIREFOX_115:
-                firefoxOptions.addArguments("--use-fake-ui-for-media-stream");
-                firefoxOptions.setCapability("browserVersion", browserVersion);
-                return firefoxOptions;
+//            case FIREFOX_114:
+//            case FIREFOX_115:
+//                firefoxOptions.addArguments("--use-fake-ui-for-media-stream");
+//                firefoxOptions.setCapability("browserVersion", browserVersion);
+//                return firefoxOptions;
             default:
                 throw new EnumNotSupportedException(this);
         }
